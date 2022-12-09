0705865f81da2a2154174518acda9d38865b28cb
Nik181 <N_bolliger45@hotmail.com>
2022-12-09 16:12:17 +0100

    fin

--- src/util/Const.java
+++ src/util/Const.java
@@ -2,9 +2,10 @@ package util;
 
 public class Const {
 
-    final public static int MAX_ORDER_PER_DAY = 10;
-    final public static int MIN_ORDER_PER_DAY = 4;
-    final public static int MAX_ARTICLES_PER_ORDER = 15;
-    final public static int MIN_ARTICLES_PER_ORDER = 5;
+    public static final int MAX_ORDER_PER_DAY = 10;
+    public static final int MIN_ORDER_PER_DAY = 4;
+    public static final int MAX_ARTICLES_PER_ORDER = 15;
+    public static final int MIN_ARTICLES_PER_ORDER = 5;
 
+    public static final int RANDOM_OFF_SEASON_NUM = 15;
 }
